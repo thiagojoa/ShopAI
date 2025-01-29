01bdb1387706c07444d3c8a6286b4cf95599e722
Thiago B. <36665874+thiagojoa@users.noreply.github.com>
2025-01-28 23:18:48 -0300

    Initial Confs for first agent: Title Optimization

--- shopai_seo_agent/src/shopai_seo_agent/crew.py
+++ shopai_seo_agent/src/shopai_seo_agent/crew.py
@@ -1,5 +1,6 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
+
 
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
@@ -15,36 +16,35 @@ class ShopaiSeoAgent():
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
+	@before_kickoff
+	def before_kickoff_function(self, inputs):
+		print(f"Before kickoff function with inputs: {inputs}")
+		return inputs # You can return the inputs or modify them as needed
+
+	@after_kickoff
+	def after_kickoff_function(self, result):
+		print(f"After kickoff function with result: {result}")
+		return result # You can return the result or modify it as needed
+
 	# If you would like to add tools to your agents, you can learn more about it here:
 	# https://docs.crewai.com/concepts/agents#agent-tools
 	@agent
-	def researcher(self) -> Agent:
+	def title_optimizer(self) -> Agent:
 		return Agent(
-			config=self.agents_config['researcher'],
-			verbose=True
-		)
-
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
+			config=self.agents_config['title_optimizer'],
+			#tools=[], # Add tools here
+			verbose=True,
+			llm=LLM(model='gemini/gemini-2.0-flash-exp')
 		)
 
 	# To learn more about structured task outputs, 
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
 	@task
-	def research_task(self) -> Task:
+	def title_optimizer_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['research_task'],
-		)
-
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			config=self.tasks_config['title_optimizer_task'],
+			output_file='output/report.md' # This is the file that will be contain the final report.
 		)
 
 	@crew
